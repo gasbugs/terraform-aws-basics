ac8032340726a1ca89e75162c7c32dbbaa882aff
Ilsun Choi <ilsunchoi@cloudsecuritylab.co.kr>
2025-03-08 10:35:59 +0900

    Update variables.tf

--- 02_HCL/terraform-module/modules/vpc/variables.tf
+++ 02_HCL/terraform-module/modules/vpc/variables.tf
@@ -1,9 +1,11 @@
-variable "vpc_name" {
-  description = "VPC의 이름" # VPC의 이름을 지정하는 변수
-  type        = string    # 변수 타입은 문자열
+variable "cidr_block" {
+  description = "VPC의 CIDR 블록"
+  type        = string
+  default     = "192.168.0.0/16"
 }
 
-variable "cidr_block" {
-  description = "VPC의 CIDR 블록" # VPC의 CIDR 블록을 지정하는 변수
-  type        = string         # 변수 타입은 문자열
+variable "vpc_name" {
+  description = "VPC의 이름"
+  type        = string
+  default     = "your-vpc"
 }
